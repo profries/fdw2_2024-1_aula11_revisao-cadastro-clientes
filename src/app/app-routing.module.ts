9f48dcc113badd91768db8a4279e945e6bb4a9fb
Luis Henrique Ries <prof.ries@gmail.com>
2024-06-13 19:57:55 -0300

    Cadastro de clientes com rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormClientesComponent } from './form-clientes/form-clientes.component';
+import { TabelaClientesComponent } from './tabela-clientes/tabela-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'tabela', component: TabelaClientesComponent},
+  { path:'cadastro', component: FormClientesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
